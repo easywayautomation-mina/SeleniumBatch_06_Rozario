8a7b1497488b93b37d52babecd0488baf9a512d0
easywayautomaiton-mina <easywayautomation.mina@gmail.com>
2020-08-23 19:22:09 -0400

    added switch

--- src/main/java/Day_03/Ex_01_Name.java
+++
@@ -1,5 +0,0 @@
-package Day_03;
-
-public class Ex_01_Name {
-
-}
---
+++ src/main/java/Day_03/Ex_01_Switch_01.java
@@ -0,0 +1,13 @@
+package Day_03;
+
+public class Ex_01_Switch_01 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+}
