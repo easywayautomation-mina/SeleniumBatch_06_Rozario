40b3319f4f1687c1219dca242e1638197901509a
easywayautomaiton-mina <easywayautomation.mina@gmail.com>
2020-08-23 21:15:34 -0400

    Ex_01_double

---
+++ src/main/java/Day_01/Ex_01_double.java
@@ -0,0 +1,13 @@
+package Day_01;
+
+public class Ex_01_double {
+    public static void main(String[] args) {
+    	int num = 4;
+    	if (num==2) {
+    		System.out.println(num);
+    	} else {
+    		System.out.println(num);
+    	}
+    }
+}
+ 
--- src/main/java/Day_02/Ex_02_IfElse.java
+++ src/main/java/Day_02/Ex_02_IfElse.java
@@ -1,5 +1,8 @@
 package Day_02;
 
 public class Ex_02_IfElse {
-
+	
+	public static void main(String[] args) {
+		
+	}
 }
